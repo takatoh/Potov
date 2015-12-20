238cc4910ea2fdc56a7120fcf24ac29c69cf1952
takatoh <takatoh.m@gmail.com>
2015-12-20 20:35:53 +0900

    app.rb: static folder.

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class PhotoViewApp < Sinatra::Base
   set :run, true
 
   enable :static
-  set :public_dir, File.dirname(__FILE__) + "/public"
+#  set :public_dir, File.dirname(__FILE__) + "/public"
   enable :methodoverride
   enable :sessions
 
