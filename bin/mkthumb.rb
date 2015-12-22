179d1e80bb639ab68b7a558a701b9f26c15e80f3
takatoh <takatoh.m@gmail.com>
2015-12-22 22:14:20 +0900

    bin/mkthumb.rb: New.

---
+++ bin/mkthumb.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+
+require './boot'
+require 'thumbnail_pool'
+
+
+dir = ARGV.shift
+pool = ThumbnailPool.new(PV_CONFIG["thumbnail_dir"], PV_CONFIG["photo_dir"])
+pool.make_r(dir)
