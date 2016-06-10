8da9abb0aba73944c24bf393d6aa501dbeb9c4ac
takatoh <takatoh.m@gmail.com>
2016-06-10 11:46:56 +0900

    bin/mkthumbs.rb: --clean option.

--- bin/mkthumbs.rb
+++ bin/mkthumbs.rb
@@ -2,10 +2,22 @@
 # encoding: utf-8
 
 
+require 'optparse'
 require './boot'
 require './lib/thumbnail_pool'
 
 
+options = {}
+opts = OptionParser.new
+opts.on("-c", "--clean", "Clean unused thumbnails."){|v| options[:clean] = true }
+opts.on_tail("-h", "--help", "Show this message."){|v| puts opts; exit }
+opts.parse!
+
 dir = ARGV.shift || PV_CONFIG["photo_dir"]
 pool = ThumbnailPool.new(PV_CONFIG["thumbnail_dir"], PV_CONFIG["photo_dir"])
-pool.make_r(dir).each{|t| puts t }
+
+if options[:clean]
+  pool.clean
+else
+  pool.make_r(dir).each{|t| puts t }
+end
