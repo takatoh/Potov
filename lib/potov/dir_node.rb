dfc3b5b4c60a41138f90ba2cb243853dee90aac4
takatoh <takatoh.m@gmail.com>
2019-11-08 03:39:27 +0900

    lib/potov/*.rb: Intu Potov module.

--- lib/potov/dir_node.rb
+++ lib/potov/dir_node.rb
@@ -6,104 +6,106 @@ require 'find'
 require 'photo_types'
 
 
-class DirNode
+mudule Potov
+  class DirNode
 
-  attr_reader :path
+    attr_reader :path
 
-  def initialize(path, root = false, root_path = nil)
-    @path = path
-    @root = root
-    @root_path = root_path ? root_path : path
-    @children = []
-    @photo_count = 0
-  end
-
-  def self.read(dir)
-    root = DirNode.new(dir, true)
-    Find.find(dir) do |f|
-      if File.directory?(f) && f != dir
-        root.add(f)
-      elsif File.file?(f) && PhotoTypes.photo?(f)
-        root.inc(File.dirname(f))
-      end
+    def initialize(path, root = false, root_path = nil)
+      @path = path
+      @root = root
+      @root_path = root_path ? root_path : path
+      @children = []
+      @photo_count = 0
     end
-    root
-  end
+
+    def self.read(dir)
+      root = DirNode.new(dir, true)
+      Find.find(dir) do |f|
+        if File.directory?(f) && f != dir
+          root.add(f)
+        elsif File.file?(f) && PhotoTypes.photo?(f)
+          root.inc(File.dirname(f))
+        end
+      end
+      root
+    end
 
 
-  def name
-    @path.split("/")[-1]
-  end
+    def name
+      @path.split("/")[-1]
+    end
 
-  def rel_path
-    @path.sub("#{@root_path}/", "")
-  end
+    def rel_path
+      @path.sub("#{@root_path}/", "")
+    end
 
-  def add(path)
-    path2 = path.sub(/#{@path}\/?/, "")
-    unless path2.empty? || path2.start_with?(".")
-      m = path2.split("/")
-      ch = if @children.map{|c| c.name }.include?(m[0])
-        @children.select{|c| c.name == m[0] }.first
+    def add(path)
+      path2 = path.sub(/#{@path}\/?/, "")
+      unless path2.empty? || path2.start_with?(".")
+        m = path2.split("/")
+        ch = if @children.map{|c| c.name }.include?(m[0])
+          @children.select{|c| c.name == m[0] }.first
+        else
+          c = DirNode.new(path, false, @root_path)
+          @children << c
+          c
+        end
+        ch.add(path)
+      end
+      self
+    end
+
+    def inc(path)
+      path2 = path.sub(/#{@path}\/?/, "")
+      unless path2.empty? || path2.start_with?(".")
+        m = path2.split("/")
+        ch = @children.select{|c| c.name == m[0] }.first
+        ch.inc(path)
       else
-        c = DirNode.new(path, false, @root_path)
-        @children << c
-        c
+        @photo_count += 1
       end
-      ch.add(path)
     end
-    self
-  end
 
-  def inc(path)
-    path2 = path.sub(/#{@path}\/?/, "")
-    unless path2.empty? || path2.start_with?(".")
-      m = path2.split("/")
-      ch = @children.select{|c| c.name == m[0] }.first
-      ch.inc(path)
-    else
-      @photo_count += 1
-    end
-  end
-
-  def to_tree(ind = 0)
-    result = ""
-    result << " " * ind + "#{name}\n"
-    result << @children.map do |ch|
-      ch.to_tree(ind + 2)
-    end.join("")
-    result
-  end
-
-  def to_html(ind: 0, include_root: true)
-    ind = ind - 4 unless include_root
-    result = ""
-    result << " " * ind + "<ul>\n" if @root && include_root
-    if include_root
-      result << " " * (ind + 2) + "<li data-path='#{rel_path}'>#{name}"
-      result << " (#{@photo_count})" if @photo_count > 0
-    end
-    unless @children.empty?
-      result << "\n" if include_root
-      result << " " * (ind + 4) + "<ul>\n"
-      @children.map do |ch|
-        result << ch.to_html(ind: ind + 4)
+    def to_tree(ind = 0)
+      result = ""
+      result << " " * ind + "#{name}\n"
+      result << @children.map do |ch|
+        ch.to_tree(ind + 2)
       end.join("")
-      result << " " * (ind + 4) + "</ul>\n"
-      result << " " * (ind + 2) + "</li>\n" if include_root
-    else
-      result << "</li>\n" if include_root
+      result
     end
-    result << " " * ind + "</ul>\n" if @root && include_root
-    result
-  end
 
-end   # of class DirNode
+    def to_html(ind: 0, include_root: true)
+      ind = ind - 4 unless include_root
+      result = ""
+      result << " " * ind + "<ul>\n" if @root && include_root
+      if include_root
+        result << " " * (ind + 2) + "<li data-path='#{rel_path}'>#{name}"
+        result << " (#{@photo_count})" if @photo_count > 0
+      end
+      unless @children.empty?
+        result << "\n" if include_root
+        result << " " * (ind + 4) + "<ul>\n"
+        @children.map do |ch|
+          result << ch.to_html(ind: ind + 4)
+        end.join("")
+        result << " " * (ind + 4) + "</ul>\n"
+        result << " " * (ind + 2) + "</li>\n" if include_root
+      else
+        result << "</li>\n" if include_root
+      end
+      result << " " * ind + "</ul>\n" if @root && include_root
+      result
+    end
+
+  end   # of class DirNode
+end   # of module Potov
 
 
 if __FILE__ == $0
   dir = ARGV.shift
-  root = DirNode.read(dir)
+  root = Potov::DirNode.read(dir)
   print root.to_html
 #  print root.to_html(ind: 0, include_root: false)
 end
--- lib/potov/thumbnail_pool.rb
+++ lib/potov/thumbnail_pool.rb
@@ -6,86 +6,88 @@ require 'fileutils'
 require 'photo_types'
 
 
-class ThumbnailPool
+module Potov
+  class ThumbnailPool
 
-  def initialize(thumbnail_dir, photo_dir)
-    @dir = thumbnail_dir
-    @photo_dir = photo_dir
-  end
+    def initialize(thumbnail_dir, photo_dir)
+      @dir = thumbnail_dir
+      @photo_dir = photo_dir
+    end
 
-  # Get thumbnail path.
-  def get(thumb_path)
-    thumbnail = File.join(@dir, thumb_path)
-    unless File.exist?(thumbnail)
-      PhotoTypes::PHOTO_TYPES.each do |t|
-        ext = File.extname(thumb_path)
-        photo = File.join(@photo_dir, thumb_path.sub(ext, t))
-        if File.exist?(photo)
-          make(photo.sub("#{@photo_dir}/", ""))
-          break
+    # Get thumbnail path.
+    def get(thumb_path)
+      thumbnail = File.join(@dir, thumb_path)
+      unless File.exist?(thumbnail)
+        PhotoTypes::PHOTO_TYPES.each do |t|
+          ext = File.extname(thumb_path)
+          photo = File.join(@photo_dir, thumb_path.sub(ext, t))
+          if File.exist?(photo)
+            make(photo.sub("#{@photo_dir}/", ""))
+            break
+          end
+        end
+      end
+      thumbnail
+    end
+
+    # Get thumbnail url path.
+    def get_url_path(path)
+      ext = File.extname(path)
+      "/thumbnail/#{path.sub(ext, ".jpg")}"
+    end
+
+    # Make thumbnail.
+    def make(path)
+      photo_path = "#{@photo_dir}/#{path}"
+      return nil unless PhotoTypes.photo?(photo_path)
+      ext = File.extname(path)
+      thumb_path = "#{@dir}/#{path.sub(ext, ".jpg")}"
+      thumb = Pathname.new(thumb_path)
+      unless thumb.exist?
+        parent = thumb.parent
+        parent.mkpath unless parent.exist?
+        system("convert -scale 90x90 -flatten #{photo_path} #{thumb_path}")
+        thumb_path.sub("#{@dir}/", "")
+      else
+        nil
+      end
+    end
+
+    # Make thumbnail of photos in specified directory.
+    def make_r(dir)
+      thumbs = []
+      if dir == @photo_dir
+        dir = Pathname.new(@photo_dir)
+      else
+        dir = Pathname.new("#{@photo_dir}/#{dir}")
+      end
+      dir.find do |f|
+        if f.file?
+          thumbs << make(f.relative_path_from(Pathname.new(@photo_dir)).to_s)
+        end
+      end
+      thumbs.compact
+    end
+
+    # Clean unused thumbnails.
+    def clean
+      thumbs = []
+      Pathname.new(@photo_dir).find do |f|
+        if f.file?
+          ext = File.extname(f)
+          thumbs << f.sub("#{@photo_dir}/", "").sub(ext, ".jpg")
+        end
+      end
+      Pathname.new(@dir).find do |f|
+        if f.file?
+          f2 = f.sub("#{@dir}/", "")
+          unless thumbs.include?(f2)
+            puts f2
+            FileUtils.rm(f)
+          end
         end
       end
     end
-    thumbnail
-  end
 
-  # Get thumbnail url path.
-  def get_url_path(path)
-    ext = File.extname(path)
-    "/thumbnail/#{path.sub(ext, ".jpg")}"
-  end
-
-  # Make thumbnail.
-  def make(path)
-    photo_path = "#{@photo_dir}/#{path}"
-    return nil unless PhotoTypes.photo?(photo_path)
-    ext = File.extname(path)
-    thumb_path = "#{@dir}/#{path.sub(ext, ".jpg")}"
-    thumb = Pathname.new(thumb_path)
-    unless thumb.exist?
-      parent = thumb.parent
-      parent.mkpath unless parent.exist?
-      system("convert -scale 90x90 -flatten #{photo_path} #{thumb_path}")
-      thumb_path.sub("#{@dir}/", "")
-    else
-      nil
-    end
-  end
-
-  # Make thumbnail of photos in specified directory.
-  def make_r(dir)
-    thumbs = []
-    if dir == @photo_dir
-      dir = Pathname.new(@photo_dir)
-    else
-      dir = Pathname.new("#{@photo_dir}/#{dir}")
-    end
-    dir.find do |f|
-      if f.file?
-        thumbs << make(f.relative_path_from(Pathname.new(@photo_dir)).to_s)
-      end
-    end
-    thumbs.compact
-  end
-
-  # Clean unused thumbnails.
-  def clean
-    thumbs = []
-    Pathname.new(@photo_dir).find do |f|
-      if f.file?
-        ext = File.extname(f)
-        thumbs << f.sub("#{@photo_dir}/", "").sub(ext, ".jpg")
-      end
-    end
-    Pathname.new(@dir).find do |f|
-      if f.file?
-        f2 = f.sub("#{@dir}/", "")
-        unless thumbs.include?(f2)
-          puts f2
-          FileUtils.rm(f)
-        end
-      end
-    end
-  end
-
-end   # of class ThumbnailPool
+  end   # of class ThumbnailPool
+end   # of mudule Potov
