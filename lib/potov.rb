a816d7d84c659d1454047a98496f4d297981bfbc
takatoh <takatoh.m@gmail.com>
2019-11-08 20:27:47 +0900

    lib/potov.rb: Add attr accessor to Potov module.

--- lib/potov.rb
+++ lib/potov.rb
@@ -3,3 +3,12 @@ require 'potov/dir_node'
 require 'potov/thumbnail_pool'
 require 'potov/photo_types'
 require 'potov/app'
+
+
+module Potov
+
+  class << self
+    attr_accessor :site_title, :photo_dir, :thumbnail_dir
+  end
+
+end   # of module Potov
